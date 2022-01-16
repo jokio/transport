9deee25383e241c343bb8e3cdb357e75c1bdab21
Ezeki Zibzibadze <ez@jok.io>
2022-01-16 14:26:28 +0400

    feat: add build_npm script

---
+++ build_npm.ts
@@ -0,0 +1,36 @@
+import { build } from 'https://deno.land/x/dnt@0.16.0/mod.ts'
+
+await build({
+  entryPoints: ['./mod.ts'],
+  outDir: './npm',
+  compilerOptions: {},
+  typeCheck: false,
+  shims: {
+    // see JS docs for overview and more options
+    deno: true,
+  },
+  mappings: {
+    'https://deno.land/x/nats@v1.4.0/src/mod.ts': {
+      name: 'nats',
+      version: '2.4.0',
+    },
+  },
+  package: {
+    // package.json properties
+    name: 'jok_transport',
+    version: Deno.args[0],
+    description: '🚌 Transport, for your messages',
+    license: 'MIT',
+    repository: {
+      type: 'git',
+      url: 'git+https://github.com/jokio/transport.git',
+    },
+    bugs: {
+      url: 'https://github.com/jokio/transport/issues',
+    },
+  },
+})
+
+// post build steps
+Deno.copyFileSync('LICENSE', 'npm/LICENSE')
+Deno.copyFileSync('README.md', 'npm/README.md')
