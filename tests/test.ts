708de8312441dea63d1d8ea6a1c80628c4c1fbc1
Ezeki Zibzibadze <ez@jok.io>
2024-04-19 19:35:43 +0400

    chore: update

--- tests/test.ts
+++ tests/test.ts
@@ -1,14 +1,14 @@
-import { nats } from '../deps.ts'
-import { NatsTransport } from '../src/nats.transport.ts'
-import { testSuite_01 } from './transport/01_connect.ts'
-import { testSuite_02 } from './transport/02_publish.ts'
-import { testSuite_03 } from './transport/03_execute.ts'
+// import { nats } from '../deps.ts'
+// import { NatsTransport } from '../src/nats.transport.ts'
+// import { testSuite_01 } from './transport/01_connect.ts'
+// import { testSuite_02 } from './transport/02_publish.ts'
+// import { testSuite_03 } from './transport/03_execute.ts'
 
-const transport = new NatsTransport({
-  natsServerUrls: ['nats://127.0.0.1:4222'],
-  StringCodec: nats.StringCodec,
-  connect: nats.connect,
-})
+// const transport = new NatsTransport({
+//   natsServerUrls: ['nats://127.0.0.1:4222'],
+//   StringCodec: nats.StringCodec,
+//   connect: nats.connect,
+// })
 
 // testSuite_01(transport)
 // testSuite_02(transport)
